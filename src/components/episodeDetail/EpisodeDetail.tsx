4f7ca3831c7cbc8e2dfb905e36a6e2ac07d32733
Edgar <edgarrodriguezbello@gmail.com>
2024-06-15 21:48:15 +0200

    feat: missing  px to em for accesibility, adapted the description of the podcast episode to follow the guidelines of the test

--- src/components/episodeDetail/EpisodeDetail.tsx
+++ src/components/episodeDetail/EpisodeDetail.tsx
@@ -1,21 +1,39 @@
 import { Result } from '../../model/PodcastDetailModel';
+import Styles from './EpisodeDetail.module.css';
 
 interface EpisodeDetailProps {
   detail: Result;
 }
 
 const EpisodeDetail = ({ detail }: EpisodeDetailProps) => {
+  const formatDescription = (description: string) => {
+    const urlRegex =
+      /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|]|(\b[\w.-]*\.(com|net|fm)\b[\w/-]*))/gi;
+
+    const descriptionAdapted = description.replace(urlRegex, (url) => {
+      const href = url.startsWith('http') ? url : `http://${url}`;
+      return `<a href="${href}" target="_blank" class="text-blue-700">${url}</a>`;
+    });
+
+    return descriptionAdapted
+      .split(/\\n\\n|\\n|\n/)
+      .map((paragraph) => `<p>${paragraph}</p>`)
+      .join('');
+  };
+
   return (
-    <div className="episode-details">
+    <div className={Styles.episodeDetail}>
       <h2>{detail.trackName}</h2>
       <p
         dangerouslySetInnerHTML={{
-          __html: detail.description ? detail.description : '',
+          __html: detail.description
+            ? formatDescription(detail.description)
+            : '',
         }}
       ></p>
       <hr />
-      <audio controls>
-        <source src={detail.trackViewUrl} type="audio/mpeg" />
+      <audio controls className={Styles.audioPlay}>
+        <source src={detail.episodeUrl} type="audio/mpeg" />
         Your browser does not support the audio element.
       </audio>
     </div>
